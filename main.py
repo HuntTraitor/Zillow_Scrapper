c957a41811acb7dedd9550d7a27f8772cf8b0f67
Hunter Tratar <hunterrrisatratar@gmail.com>
2023-08-21 13:29:22 -0700

    reformated

--- main.py
+++ main.py
@@ -8,16 +8,22 @@ from datetime import date
 url='https://www.zillow.com/seattle-wa/{page}_p/'
 lst=[]
 today = date.today()
-for page in range(1,21):
-    r = requests.get(url.format(page=page),headers = {'User-Agent':'Mozilla/5.0'})
-    data = json.loads(re.search(r'<script id="__NEXT_DATA__" type="application/json">(\{"props".*?)</script>', r.text).group(1))
 
-    for item in data['props']['pageProps']['searchPageState']['cat1']['searchResults']['listResults']:
-        price= item['price']
-        street=item['addressStreet'] 
-        zip=item['addressZipcode']
-        sqrft=item['area']
-        date=today
-        lst.append({'date': date, 'address': street, 'price': price, 'zip': zip, 'SQRFT': sqrft})
-df = pd.DataFrame(lst).to_csv('out.csv', index=False)
-print(pd.DataFrame(lst))
+def getData(url):
+    for page in range(1,21):
+        r = requests.get(url.format(page=page),headers = {'User-Agent':'Mozilla/5.0'})
+        data = json.loads(re.search(r'<script id="__NEXT_DATA__" type="application/json">(\{"props".*?)</script>', r.text).group(1))
+
+        for item in data['props']['pageProps']['searchPageState']['cat1']['searchResults']['listResults']:
+            price= item['price']
+            street=item['addressStreet'] 
+            zip=item['addressZipcode']
+            sqrft=item['area']
+            date=today
+            lst.append({'date': date, 'address': street, 'price': price, 'zip': zip, 'SQRFT': sqrft})
+    return lst
+
+
+data = getData(url)
+print(pd.DataFrame(data))
+
