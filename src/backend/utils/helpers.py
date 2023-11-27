4fd79838c96172dc94170eca02ba6c9383832e30
hunttraitor <hunterrrisatratar@gmail.com>
2023-11-27 14:04:41 -0800

    spelling fix and header change

--- src/backend/utils/helpers.py
+++ src/backend/utils/helpers.py
@@ -24,7 +24,8 @@ def getData(url):
     lst=[]
     today = date.today()
     for page in range(1,21):
-        r = requests.get(url.format(page=page),headers = {'User-Agent':'Mozilla/5.0'})
+        r = requests.get(url.format(page=page),headers = {'User-Agent':'AppleWebKit/605.1.15'})
+        print(r)
         data = json.loads(re.search(r'<script id="__NEXT_DATA__" type="application/json">(\{"props".*?)</script>', r.text).group(1))
         for item in data['props']['pageProps']['searchPageState']['cat1']['searchResults']['listResults']:
             price= item['price']
